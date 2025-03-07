93817463644508b2432e81e3c9efd2d48d941dcc
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-07 12:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// let cnt=1
 function insert_Row() {
     //Write your code here
-  
+	let table=document.getElementById('sampleTable');
+	table.innerHTML=`
+		<tr>
+			<td>New Cell1</td>
+			<td>New Cell2</td>
+		</tr>
+	`+table.innerHTML;
+	
   
 }
